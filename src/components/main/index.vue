3d0c6520e671aef1b69e40aafb7d1899b6d123e4
milkcat1994 <milkcat1994@gmail.com>
2021-08-05 21:56:07 +0900

    Update main, projectCard
    update projectCard style
    update project img and link

--- src/components/main/index.vue
+++ src/components/main/index.vue
@@ -1,9 +1,8 @@
 <template>
   <div id="main">
-      <ProjectCard/>
-      <ProjectCard/>
-      <ProjectCard/>
-      <ProjectCard/>
+      <ProjectCard v-for="(project, key) in projects" v-bind:key="key"
+      v-bind:pjt="project"
+      />
   </div>
 </template>
 
@@ -11,6 +10,27 @@
 import ProjectCard from '@/components/projectCard/index.vue'
 
 export default {
+  data () {
+    return {
+      projects: [
+        {
+          num: 1,
+          img: 'https://user-images.githubusercontent.com/18321002/128350386-a12e0272-37fc-40cb-8dde-5397fdaeb88b.png',
+          url: 'https://github.com/milkcat1994/SSAFYPJT_1'
+        },
+        {
+          num: 2,
+          img: 'https://user-images.githubusercontent.com/18321002/128350389-bbaae485-2774-4972-819a-4f38a2e2f451.png',
+          url: 'https://github.com/milkcat1994/SSAFYPJT_2'
+        },
+        {
+          num: 3,
+          img: 'https://user-images.githubusercontent.com/18321002/128350391-5d291713-a6d2-45dd-be67-faa239b8a10e.png',
+          url: 'https://github.com/milkcat1994/SSAFYPJT_3'
+        }
+      ]
+    }
+  },
   components: {
     ProjectCard
   }
--- src/components/projectCard/index.vue
+++ src/components/projectCard/index.vue
@@ -1,16 +1,29 @@
 <template>
   <div class="dp-block card p-10">
-    <div class="border-b2">
-        <div>
-            project
-        </div>
+    <div class="border-b2"
+    v-on:click="moveProjectGitHub">
+      <!-- <img v-bind:src="project.img" class="card-img"> -->
+      <img :src="require('@/assets/pjt/'+project.num+'.png')" class="card-img">
     </div>
   </div>
 </template>
 
 <script>
 export default {
+  props: [
+    'pjt'
+  ],
+  data () {
+    return {
+      project: this.pjt
+    }
+  },
   methods: {
+    moveProjectGitHub: function () {
+      window.open(this.project.url)
+    }
+  },
+  created () {
   }
 }
 </script>
